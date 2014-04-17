ca86710bdb0f39576ec7691f7f6f7ed7a6e737e1
root <alevellop@gmail.com>
2014-04-17 19:04:51 +0200

    primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Zurbrails::Application.config.secret_key_base = '6e57cf6ef4c24b9b1aaa71afbc1d8a96b1740be89f35512809ad3c50d682233e725fc6baa0bedcf77f5a3f2cae5a244afb08312574d27ba991fb02ed899c9eef'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Zurbrails::Application.config.session_store :cookie_store, key: '_zurbrails_session'
